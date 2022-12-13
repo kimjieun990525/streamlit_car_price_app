7ff2d9d34a77c23a8182078a246b44752c4cbceb
kimjieun990525 <120348534+kimjieun990525@users.noreply.github.com>
2022-12-13 17:59:41 +0900

    인공지능

--- app.py
+++ app.py
@@ -1,27 +1,24 @@
 import streamlit as st
-from app_home import run_app_home
-from app_home import run_app_eda
+from app_home import run_home_app
+from app_eda import run_eda_app
+from app_ml import run_ml_app
 
-def main() :
+def main():
     
     st.title('자동차 가격 예측 앱')
 
     menu = ['Home', 'EDA', 'ML']
-    st.sidebar.selectbox('메뉴', menu)
+    choice = st.sidebar.selectbox('메뉴', menu)
 
-    if choice == 'Home' :
-        run_app_home()
-    if choice == 'EDA' :
-        run_app_eda()
-    if choice == 'ML' :
-        pass
-
-
-
+    if choice == 'Home':
+        run_home_app()
+    elif choice == 'EDA' :
+        run_eda_app()
+    elif choice == 'ML' :
+        run_ml_app()
 
 if __name__ == '__main__' :
     main()
-
 
 
 
--- app_home.py
+++ app_home.py
@@ -5,4 +5,4 @@ def run_home_app():
     st.text('데이터분석 및 고객 정보를 넣으면, 얼마정도의 차를 구매할지를 예측해 줍니다.')
     
     img_url = 'https://www.motorgraph.com/news/photo/201905/22564_72789_5839.jpg'
-    st.image(img_url)
+    st.image(img_url)   
